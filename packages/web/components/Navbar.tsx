68bdfa9f8e3c5bfa0fced4adc502194928f5db0c
haoyudong <2070343054@email.szu.edu.cn>
2022-10-11 23:22:43 +0800

    update navbar

--- packages/web/components/Navbar.tsx
+++ packages/web/components/Navbar.tsx
@@ -7,43 +7,117 @@ import {
 } from '../contexts/auth';
 
 export const Navbar = () => {
-  const { logout } = useAuth();
+  const { user, logout } = useAuth();
   return (
-    <div className="w-full flex justify-between py-4">
-      <div className="flex gap-4">
-        <div className="px-5 py-2  bg-white font-bold rounded">
-          <Link href="/">
-            <a>Home</a>
+    <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+      <div className="container flex flex-wrap justify-between items-center mx-auto">
+        <a href="https://flowbite.com/" className="flex items-center">
+          <div className="mr-3 h-6 sm:h-9"></div>
+          <Link href="/" passHref>
+            <button className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+              Follow
+            </button>
           </Link>
+        </a>
+        <button
+          data-collapse-toggle="navbar-default"
+          type="button"
+          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-default"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-6 h-6"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </button>
+        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <UnauthenticatedOnly>
+              <>
+                <li>
+                  <Link href="/signup" passHref>
+                    <button className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                      Sign Up
+                    </button>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/login" passHref>
+                    <button className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                      Log In
+                    </button>
+                  </Link>
+                </li>
+              </>
+            </UnauthenticatedOnly>
+            <AuthenticatedOnly>
+              <>
+                <li>
+                  <p className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white">
+                    {user?.name}
+                  </p>
+                </li>
+                <li>
+                  <button
+                    onClick={logout}
+                    className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+                  >
+                    Log out
+                  </button>
+                </li>
+              </>
+            </AuthenticatedOnly>
+          </ul>
         </div>
       </div>
-      <UnauthenticatedOnly>
-        <div className="flex">
-          <div className="ml-10 px-5 py-2  bg-white font-bold rounded">
-            <Link href="/signup">
-              <a>Sign Up</a>
-            </Link>
-          </div>
-          <div className="ml-10 px-5 py-2  bg-white font-bold rounded">
-            <Link href="/login">
-              <a>Log In</a>
-            </Link>
-          </div>
-        </div>
-      </UnauthenticatedOnly>
-      <AuthenticatedOnly>
-        <div className="flex">
-          <div className="ml-10 px-5 py-2  bg-white font-bold rounded">
-            <UserInfo />
-          </div>
-          <div className="ml-10 px-5 py-2  bg-white font-bold rounded">
-            <button className="font-bold" onClick={logout}>
-              Log Out
-            </button>
-          </div>
-        </div>
-      </AuthenticatedOnly>
-    </div>
+    </nav>
+
+    // <div classNameName="w-full flex justify-between py-4">
+    //   <div classNameName="flex gap-4">
+    //     <div classNameName="px-5 py-2  bg-white font-bold rounded">
+    //       <Link href="/">
+    //         <a>Home</a>
+    //       </Link>
+    //     </div>
+    //   </div>
+    //   <UnauthenticatedOnly>
+    //     <div classNameName="flex">
+    //       <div classNameName="ml-10 px-5 py-2  bg-white font-bold rounded">
+    //         <Link href="/signup">
+    //           <a>Sign Up</a>
+    //         </Link>
+    //       </div>
+    //       <div classNameName="ml-10 px-5 py-2  bg-white font-bold rounded">
+    //         <Link href="/login">
+    //           <a>Log In</a>
+    //         </Link>
+    //       </div>
+    //     </div>
+    //   </UnauthenticatedOnly>
+    //   <AuthenticatedOnly>
+    // <div classNameName="flex">
+    //   <div classNameName="ml-10 px-5 py-2  bg-white font-bold rounded">
+    //     <UserInfo />
+    //   </div>
+    //   <div classNameName="ml-10 px-5 py-2  bg-white font-bold rounded">
+    //     <button classNameName="font-bold" onClick={logout}>
+    //       Log Out
+    //     </button>
+    //   </div>
+    // </div>
+    // </AuthenticatedOnly>
+    // </div>
   );
 };
 
