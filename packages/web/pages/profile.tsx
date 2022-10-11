c64b650445e2c6917af5a2c08b4f6ca5d4c27845
haoyudong <2070343054@email.szu.edu.cn>
2022-10-11 22:15:48 +0800

    protect router

--- packages/web/pages/profile.tsx
+++ packages/web/pages/profile.tsx
@@ -3,8 +3,7 @@ import { useQueryClient } from 'react-query';
 import { trpc } from '../api/APIProvider';
 import Follow from '../components/Follow';
 import type { NextPage } from 'next';
-import useRequiresAuth from '../hooks/useRequiresAuth';
-import { useAuth } from '../contexts/auth';
+import { AuthenticatedOnly, useAuth } from '../contexts/auth';
 const Profile: NextPage = () => {
   const { user, isAuthenticated } = useAuth();
   const queryClient = useQueryClient();
@@ -35,136 +34,136 @@ const Profile: NextPage = () => {
     });
     return flag;
   };
-  useRequiresAuth();
   return (
     <MainLayout>
       <Card>
-        <>
-          <h1 className="font-black text-3xl mb-10">{user?.name} Profile</h1>
-          <h1 className="font-black text-3xl mb-10">
-            Follower : {list?.followerslist?.length}
-          </h1>
-
-          <h2 className="font-black text-3xl mb-10">
-            Following : {list?.followinglist?.length}
-          </h2>
-          <div className="flex justify-between">
-            {/* Following List */}
-            <div className="p-4 w-full max-w-md bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-              <div className="flex justify-between items-center mb-4">
-                <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
-                  Your Following Lists
-                </h5>
+        <AuthenticatedOnly>
+          <>
+            <h2 className="font-black text-3xl mb-10">{user?.name} Profile</h2>
+            <h2 className="font-black text-3xl mb-10">
+              Follower : {list?.followerslist?.length}
+            </h2>
+            <h2 className="font-black text-3xl mb-10">
+              Following : {list?.followinglist?.length}
+            </h2>
+            <div className="flex justify-between">
+              {/* Following List */}
+              <div className="p-4 w-full max-w-md bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                <div className="flex justify-between items-center mb-4">
+                  <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
+                    Your Following Lists
+                  </h5>
+                </div>
+                <div>
+                  <ul
+                    role="list"
+                    className="divide-y divide-gray-200 dark:divide-gray-700"
+                  >
+                    {list?.followinglist?.map((user) => (
+                      <>
+                        <li className="py-3 sm:py-4" key={user.following.id}>
+                          <div className="flex items-center space-x-4">
+                            <div className="flex-shrink-0">
+                              <div className="w-8 h-8 rounded-full" />
+                            </div>
+                            <div className="flex-1 min-w-0">
+                              <p className="text-m font-medium text-gray-900 truncate dark:text-white">
+                                {user.following.name}
+                              </p>
+                              <p className="text-m text-gray-500 truncate dark:text-gray-400">
+                                Email: {user.following.email}
+                              </p>
+                            </div>
+                            <Follow
+                              id={user.following.id}
+                              hasFollow={hasFollow(user.following.id)}
+                              unFollowMutate={unfollowMutate}
+                              followMutate={followMutate}
+                            />
+                          </div>
+                        </li>
+                      </>
+                    ))}
+                  </ul>
+                </div>
               </div>
-              <div>
-                <ul
-                  role="list"
-                  className="divide-y divide-gray-200 dark:divide-gray-700"
-                >
-                  {list?.followinglist?.map((user) => (
-                    <>
-                      <li className="py-3 sm:py-4" key={user.following.id}>
-                        <div className="flex items-center space-x-4">
-                          <div className="flex-shrink-0">
-                            <div className="w-8 h-8 rounded-full" />
+              {/* All User List */}
+              <div className="p-4 w-full max-w-md bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                <div className="flex justify-between items-center mb-4">
+                  <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
+                    All User Lists
+                  </h5>
+                </div>
+                <div>
+                  <ul
+                    role="list"
+                    className="divide-y divide-gray-200 dark:divide-gray-700"
+                  >
+                    {users?.map((user) => (
+                      <>
+                        <li className="py-3 sm:py-4" key={user.id}>
+                          <div className="flex items-center space-x-4">
+                            <div className="flex-shrink-0">
+                              <div className="w-8 h-8 rounded-full" />
+                            </div>
+                            <div className="flex-1 min-w-0">
+                              <p className="text-m font-medium text-gray-900 truncate dark:text-white">
+                                {user.name}
+                              </p>
+                              <p className="text-m text-gray-500 truncate dark:text-gray-400">
+                                Email: {user.email}
+                              </p>
+                            </div>
+                            <Follow
+                              id={user.id}
+                              hasFollow={hasFollow(user.id)}
+                              unFollowMutate={unfollowMutate}
+                              followMutate={followMutate}
+                            />
                           </div>
-                          <div className="flex-1 min-w-0">
-                            <p className="text-m font-medium text-gray-900 truncate dark:text-white">
-                              {user.following.name}
-                            </p>
-                            <p className="text-m text-gray-500 truncate dark:text-gray-400">
-                              Email: {user.following.email}
-                            </p>
+                        </li>
+                      </>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+              {/* Follower List */}
+              <div className="p-4 w-full max-w-md bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                <div className="flex justify-between items-center mb-4">
+                  <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
+                    Your Follower Lists
+                  </h5>
+                </div>
+                <div>
+                  <ul
+                    role="list"
+                    className="divide-y divide-gray-200 dark:divide-gray-700"
+                  >
+                    {list?.followerslist?.map((user) => (
+                      <>
+                        <li className="py-3 sm:py-4" key={user.follower.id}>
+                          <div className="flex items-center space-x-4">
+                            <div className="flex-shrink-0">
+                              <div className="w-8 h-8 rounded-full" />
+                            </div>
+                            <div className="flex-1 min-w-0">
+                              <p className="text-m font-medium text-gray-900 truncate dark:text-white">
+                                {user.follower.name}
+                              </p>
+                              <p className="text-m text-gray-500 truncate dark:text-gray-400">
+                                Email: {user.follower.email}
+                              </p>
+                            </div>
                           </div>
-                          <Follow
-                            id={user.following.id}
-                            hasFollow={hasFollow(user.following.id)}
-                            unFollowMutate={unfollowMutate}
-                            followMutate={followMutate}
-                          />
-                        </div>
-                      </li>
-                    </>
-                  ))}
-                </ul>
+                        </li>
+                      </>
+                    ))}
+                  </ul>
+                </div>
               </div>
             </div>
-            {/* All User List */}
-            <div className="p-4 w-full max-w-md bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-              <div className="flex justify-between items-center mb-4">
-                <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
-                  All User Lists
-                </h5>
-              </div>
-              <div>
-                <ul
-                  role="list"
-                  className="divide-y divide-gray-200 dark:divide-gray-700"
-                >
-                  {users?.map((user) => (
-                    <>
-                      <li className="py-3 sm:py-4" key={user.id}>
-                        <div className="flex items-center space-x-4">
-                          <div className="flex-shrink-0">
-                            <div className="w-8 h-8 rounded-full" />
-                          </div>
-                          <div className="flex-1 min-w-0">
-                            <p className="text-m font-medium text-gray-900 truncate dark:text-white">
-                              {user.name}
-                            </p>
-                            <p className="text-m text-gray-500 truncate dark:text-gray-400">
-                              Email: {user.email}
-                            </p>
-                          </div>
-                          <Follow
-                            id={user.id}
-                            hasFollow={hasFollow(user.id)}
-                            unFollowMutate={unfollowMutate}
-                            followMutate={followMutate}
-                          />
-                        </div>
-                      </li>
-                    </>
-                  ))}
-                </ul>
-              </div>
-            </div>
-            {/* Follower List */}
-            <div className="p-4 w-full max-w-md bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-              <div className="flex justify-between items-center mb-4">
-                <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
-                  Your Follower Lists
-                </h5>
-              </div>
-              <div>
-                <ul
-                  role="list"
-                  className="divide-y divide-gray-200 dark:divide-gray-700"
-                >
-                  {list?.followerslist?.map((user) => (
-                    <>
-                      <li className="py-3 sm:py-4" key={user.follower.id}>
-                        <div className="flex items-center space-x-4">
-                          <div className="flex-shrink-0">
-                            <div className="w-8 h-8 rounded-full" />
-                          </div>
-                          <div className="flex-1 min-w-0">
-                            <p className="text-m font-medium text-gray-900 truncate dark:text-white">
-                              {user.follower.name}
-                            </p>
-                            <p className="text-m text-gray-500 truncate dark:text-gray-400">
-                              Email: {user.follower.email}
-                            </p>
-                          </div>
-                        </div>
-                      </li>
-                    </>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          </div>
-        </>
+          </>
+        </AuthenticatedOnly>
       </Card>
     </MainLayout>
   );
